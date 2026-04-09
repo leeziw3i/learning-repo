f3ca63e256eb2c00edc795acaebcaf51fc2e8d42
lzw <1465924180@qq.com>
2026-04-09 14:02:03 +0800

    删除非必要文件

--- test_img.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-print("Pillow 成功导入了！")
-# 创建一个简单的 100x100 红色图片
-img = Image.new('RGB', (100, 100), color = 'green')
-img.save('test.png')
-# 打印测试图片
-print("测试图片 test.png 已生成！")
